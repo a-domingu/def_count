5a31bbead40b251666454917dea242f93d64cb49
adomingu <alfonso.dominguez-de-la-iglesia-external@capgemini.com>
2020-09-11 13:00:24 +0200

    Everything works. Ignoring underscore

--- def_count/def_count.py
+++ def_count/def_count.py
@@ -8,19 +8,35 @@ withing that directory. The key to do that is to use 'os.walk'
 Then, the 'count' function will receive this list of files and it will count all the instances of 'def'
 '''
 
+def _find_def(line):
+    #because it's a filter function, it should return a boolean
+    ls_words = line.split(' ')
+    return 'def' in ls_words
+
+
+
 def get_files(path):
     ls = []
     for i in os.walk(path):
-        '''
-        #'i' is a tuple with 3 elements: i[0] is the path we're in, i[1] is the list of FOLDERS, i[2], FILES
-        file_path = os.path.join(i[0], i[2]) #i[2] ES UNA LISTA, NO UN STRING
-        ls.append(file_path)'''
         directory, ls_files = i[0], i[2]
         for file in ls_files:
             if file.endswith('.py'):
-                path = os.path.join(directory, file)
-                ls.append(path)
+                if not file.startswith('_'):
+                    path = os.path.join(directory, file)
+                    ls.append(path)
     return ls
 
 def count(ls_files):
-    pass
+    sc = SparkContext.getOrCreate()
+    num = 0
+    for file in ls_files:
+        if os.path.exists(file):
+            rdd = sc.textFile(file)
+            num += rdd.filter(_find_def).count()
+    return num
+
+'''
+path = os.path.join(os.getcwd(), 'prueba.py') #el archivo sí existe
+num = count([path, path])
+print(num)
+'''
